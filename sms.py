7264c845314c0b877651ca63c31078fca6a84b25
ojas <ojasfarm31@gmail.com>
2022-09-24 10:17:17 +0530

    made some changes in sms

--- sms.py
+++ sms.py
@@ -4,7 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 client = Courier(auth_token=os.getenv("AUTH_TOKEN"))
 def send_sms(number):
-
     client.send_message(
             message={
                 "to": {
